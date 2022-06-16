76d5e2c30824e102d9e38b0afdf8cf791db01b81
Sabbadini10 <matiassabbadini24@gmail.com>
2022-06-16 20:44:28 -0300

    agrege archivos html y rutas

--- app.js
+++ app.js
@@ -1,14 +1,43 @@
 let express = require('express');
 let app = express();
 let port = 3030
+let path = require('path')
 
 app.listen(port, () => 
 console.log("Levantando un servidor en http://localhost:" + port));
 
 app.get("/", function(req, res) {
-    res.send("Hola Mundo")
+    res.sendFile(path.join(__dirname, "view/index.html"))
 })
 
-app.get("/salida", function(req, res) {
-    res.send("Chau")
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "view/index.html"))
+})
+
+app.get("//berners-lee", function(req, res) {
+    res.sendFile(path.join(__dirname, "view//berners-lee.html"))
+})
+
+app.get("/babbage", function(req, res) {
+    res.sendFile(path.join(__dirname, "view/babbage.html"))
+})
+
+app.get("/clarke", function(req, res) {
+    res.sendFile(path.join(__dirname, "view/clarke.html"))
+})
+
+app.get("/hamilton", function(req, res) {
+    res.sendFile(path.join(__dirname, "view/hamilton.html"))
+})
+
+app.get("/hopper", function(req, res) {
+    res.sendFile(path.join(__dirname, "view/hopper.html"))
+})
+
+app.get("/lovelace", function(req, res) {
+    res.sendFile(path.join(__dirname, "view/lovelace.html"))
+})
+
+app.get("/turing", function(req, res) {
+    res.sendFile(path.join(__dirname, "view/turing.html"))
 })
