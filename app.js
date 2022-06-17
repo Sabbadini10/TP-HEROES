696a80f6075ed8981ebe1e9391ecae51226abae6
Sabbadini10 <matiassabbadini24@gmail.com>
2022-06-17 17:05:01 -0300

    agrege imagenes

--- app.js
+++ app.js
@@ -3,41 +3,25 @@ let app = express();
 let port = 3030
 let path = require('path')
 
-app.listen(port, () => 
-console.log("Levantando un servidor en http://localhost:" + port));
+app.use(express.static('public'));
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view/index.html"))
-})
+app.listen(port, () => console.log("Levantando un servidor en http://localhost:" + port));
 
-app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "view/index.html"))
-})
 
-app.get("//berners-lee", function(req, res) {
-    res.sendFile(path.join(__dirname, "view//berners-lee.html"))
-})
+app.get("/", function(req, res) { res.sendFile(path.join(__dirname, "view/index.html")) })
 
-app.get("/babbage", function(req, res) {
-    res.sendFile(path.join(__dirname, "view/babbage.html"))
-})
+app.get("/", function(req, res) { res.sendFile(path.join(__dirname, "view/index.html"))})
 
-app.get("/clarke", function(req, res) {
-    res.sendFile(path.join(__dirname, "view/clarke.html"))
-})
+app.get("//berners-lee", function(req, res) {res.sendFile(path.join(__dirname, "view//berners-lee.html"))})
 
-app.get("/hamilton", function(req, res) {
-    res.sendFile(path.join(__dirname, "view/hamilton.html"))
-})
+app.get("/babbage", function(req, res) {res.sendFile(path.join(__dirname, "view/babbage.html"))})
 
-app.get("/hopper", function(req, res) {
-    res.sendFile(path.join(__dirname, "view/hopper.html"))
-})
+app.get("/clarke", function(req, res) {res.sendFile(path.join(__dirname, "view/clarke.html"))})
 
-app.get("/lovelace", function(req, res) {
-    res.sendFile(path.join(__dirname, "view/lovelace.html"))
-})
+app.get("/hamilton", function(req, res) {res.sendFile(path.join(__dirname, "view/hamilton.html"))})
 
-app.get("/turing", function(req, res) {
-    res.sendFile(path.join(__dirname, "view/turing.html"))
-})
+app.get("/hopper", function(req, res) {res.sendFile(path.join(__dirname, "view/hopper.html"))})
+
+app.get("/lovelace", function(req, res) {res.sendFile(path.join(__dirname, "view/lovelace.html"))})
+
+app.get("/turing", function(req, res) {res.sendFile(path.join(__dirname, "view/turing.html"))})
